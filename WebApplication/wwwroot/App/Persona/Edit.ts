1f2b1db60c1d1e699b4639d2dbdaa1d1f6c7be93
Ebass29 <eliasrodriguez299@hotmail.com>
2021-07-31 21:13:52 -0600

    Add project files.

---
+++ WebApplication/wwwroot/App/Persona/Edit.ts
@@ -0,0 +1,29 @@
+﻿namespace PersonaEdit {
+
+    var Entity = $("#AppEdit").data("entity");
+
+    var Formulario = new Vue({
+
+        data: {
+            Formulario: "#FormEdit",
+            Entity: Entity,
+        },
+
+        methods: {
+            RefrescarValidaciones() {
+                BValidateData(this.Formulario);
+            }
+        },
+
+
+        mounted() {
+            CreateValidator(this.Formulario);
+        }
+
+    }
+    );
+
+    Formulario.$mount("#AppEdit");
+
+
+}
